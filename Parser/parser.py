f9136026693c92a24efcd424be82c27bd653d2c7
Zechariah McPherson <ZechariahMcPherson@gmail.com>
2018-02-26 06:06:13 -0600

    finally got some of the quoting to work

--- Parser/parser.py
+++ Parser/parser.py
@@ -3,14 +3,15 @@ import csv
 
 #takes countryInfo text file from geonames and makes csv
 def parseCountriesTxt(newFile, dataFile):
-    with open(newFile, 'wt',  newline='') as csvfile:
+    with open(newFile, 'wt',  newline='', encoding='utf-8') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=',',
-          quotechar='|' , quoting=csv.QUOTE_MINIMAL)
+          quotechar='"', quoting=csv.QUOTE_ALL)
 
-        with open(dataFile, newline='') as csvfile2:
-             spamreader = csv.reader(csvfile2, delimiter='\t', quotechar='|')
+        with open(dataFile, newline='', encoding='utf-8') as csvfile2:
+             spamreader = csv.reader(csvfile2, delimiter='\t')
              for row in spamreader:
-
+                 if(spamreader.line_num == 81):
+                    print(row)
                  #rows that don't contain data start with #
                  if(row[0][0] != "#"):
                      #print(row[0][0])
@@ -19,13 +20,16 @@ def parseCountriesTxt(newFile, dataFile):
 
 #takes the countryInfo csv and pulls specific columns
 def parseCountriesCsv(newFile, dataFile):
-    with open(newFile, 'wt',  newline='') as csvfile:
+    with open(newFile, 'wt',  newline='', encoding='utf-8') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=',',
-          quotechar='|', quoting=csv.QUOTE_MINIMAL)
+          quotechar='"', quoting=csv.QUOTE_ALL)
 
         with open(dataFile, newline='') as csvfile2:
-             spamreader = csv.reader(csvfile2, quotechar='|')
+             spamreader = csv.reader(csvfile2)
              for row in spamreader:
+                 if(spamreader.line_num == 30):
+                     print(row)
+                     print(row[4])
                  #row[0] - ISO
                  #row[1] - ISO3
                  #row[4] - country name
@@ -38,5 +42,4 @@ def parseCountriesCsv(newFile, dataFile):
 def main():
     parseCountriesTxt('test.csv','../DATA/countryInfo.txt')
     parseCountriesCsv('testFilter.csv', './test.csv')
-
 main()

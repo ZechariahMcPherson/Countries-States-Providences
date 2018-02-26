d9277a05900b89c29511a5e7b5e636466fe1e35e
Zechariah McPherson <ZechariahMcPherson@gmail.com>
2018-02-25 18:56:14 -0800

    made parseFile more generic

--- Parser/parser.py
+++ Parser/parser.py
@@ -1,17 +1,22 @@
 import csv
 
-def main():
 
-    with open('test.csv', 'wt',  newline='') as csvfile:
+def parseFile(parseToFile, fileToParse):
+    with open(parseToFile, 'wt',  newline='') as csvfile:
         spamwriter = csv.writer(csvfile,
           quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        print("hi")
 
-        with open('../Data/countryInfo.txt', newline='') as csvfile2:
+        with open(fileToParse, newline='') as csvfile2:
              spamreader = csv.reader(csvfile2, delimiter='\t', quotechar='|')
              for row in spamreader:
                  spamwriter.writerow(row)
                  print(row)
+
+
+
+def main():
+
+    parseFile('test.csv','../Data/countryInfo.txt' )
 
 
 main()

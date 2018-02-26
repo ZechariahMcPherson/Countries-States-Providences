6d528024e8a712a22f917dfbce6e511c3e06ccba
Zechariah McPherson <ZechariahMcPherson@gmail.com>
2018-02-26 02:12:36 -0800

    created csv with iso iso3 country geoid

--- Parser/parser.py
+++ Parser/parser.py
@@ -1,22 +1,42 @@
 import csv
 
 
-def parseFile(parseToFile, fileToParse):
-    with open(parseToFile, 'wt',  newline='') as csvfile:
-        spamwriter = csv.writer(csvfile,
-          quotechar='|', quoting=csv.QUOTE_MINIMAL)
+#takes countryInfo text file from geonames and makes csv
+def parseCountriesTxt(newFile, dataFile):
+    with open(newFile, 'wt',  newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',',
+          quotechar='|' , quoting=csv.QUOTE_MINIMAL)
 
-        with open(fileToParse, newline='') as csvfile2:
+        with open(dataFile, newline='') as csvfile2:
              spamreader = csv.reader(csvfile2, delimiter='\t', quotechar='|')
              for row in spamreader:
-                 spamwriter.writerow(row)
-                 print(row)
 
+                 #rows that don't contain data start with #
+                 if(row[0][0] != "#"):
+                     #print(row[0][0])
+                     #print(row)
+                     spamwriter.writerow(row)
+
+#takes the countryInfo csv and pulls specific columns
+def parseCountriesCsv(newFile, dataFile):
+    with open(newFile, 'wt',  newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',',
+          quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+        with open(dataFile, newline='') as csvfile2:
+             spamreader = csv.reader(csvfile2, quotechar='|')
+             for row in spamreader:
+                 #row[0] - ISO
+                 #row[1] - ISO3
+                 #row[4] - country name
+                 #row[16] - geoID assigned by geonames
+                 line = [row[0], row[1], row[4], row[16]]
+                 #print(line)
+                 spamwriter.writerow(line)
 
 
 def main():
-
-    parseFile('test.csv','../Data/countryInfo.txt' )
-
+    parseCountriesTxt('test.csv','../DATA/countryInfo.txt')
+    parseCountriesCsv('testFilter.csv', './test.csv')
 
 main()

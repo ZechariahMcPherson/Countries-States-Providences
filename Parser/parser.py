a9f12d34b7366ee5ef151688df30372c1b62cbfa
Zechariah McPherson <ZechariahMcPherson@gmail.com>
2018-03-17 10:44:05 -0500

    created reader and writer for country csv/txt

--- Parser/parser.py
+++ Parser/parser.py
@@ -6,22 +6,34 @@ def createFile(create):
 
     try:
         file = open(create, 'wt',  newline='', encoding='utf-8')
+        return file
 
     except OSError:
         print "Error: could not create ", create
         sys.exit()
 
+
 #error handling for opening a file
 def openFile(open):
     try:
         file = open(open, 'wt',  newline='', encoding='utf-8')
-
+        return file
+        
     except OSError:
         print "Error: could not open ", open
         sys.exit()
 
+#converts txt to csv for countryInfo.txt
+def countryTxt2Csv(destination, source):
 
+    #creates csv file and a writer to write to that file
+    newCsv = createFile(destination)
+    newCsvWriter = csv.writer(newCsv, delimiter=',',
+      quotechar='"', quoting=csv.QUOTE_ALL)
 
+    #opens data file and creates a reader for that file
+    dataTxt  = openFile(source)
+    dataTxtReader = csv.reader(dataTxt, delimiter='\t')
 
 
 
